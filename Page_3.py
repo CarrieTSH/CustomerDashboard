870995bace32605bc0a36a0e4dc968f953c68033
CarrieTSH <161866855+CarrieTSH@users.noreply.github.com>
2024-03-01 21:38:05 +0100

    Update Page_3.py

--- Page_3.py
+++ Page_3.py
@@ -24,7 +24,7 @@ st.set_page_config(
 navigation_items = {
     "Homepage": "https://sales-app-homepage1.streamlit.app/",
     "Exec Dash": "https://sales-app-execdash1.streamlit.app/",
-    "Products Dash": "https://sales-app-itemsdash1.streamlit.app/"
+    "Products Dash": "https://sales-app-itemsdash1.streamlit.app/",
     "Customers Dash":"None"
 }
 
